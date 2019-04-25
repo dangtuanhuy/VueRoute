035e4627aeb21c8f30c0d37650ea141cd4da5f49
Huy Dang Tuan <huy.dang@orientsoftware.com>
2019-04-25 17:13:38 +0700

    Form

--- src/components/myForm.vue
+++ src/components/myForm.vue
@@ -3,19 +3,23 @@
     <form>
       <div class="row">
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-          <h1>File a Complaint</h1>
+          <h1>Fill Form</h1>
           <hr>
           <div class="form-group">
             <label for="email">Mail</label>
-            <input type="text" id="email" class="form-control">
+            <input type="text" id="email" class="form-control" v-model="userData.email">
           </div>
           <div class="form-group">
             <label for="password">Password</label>
-            <input type="password" id="password" class="form-control">
+            <input
+              type="password"
+              id="password"
+              class="form-control"
+              v-model.lazy="userData.password">
           </div>
           <div class="form-group">
             <label for="age">Age</label>
-            <input type="number" id="age" class="form-control">
+            <input type="number" id="age" class="form-control" v-model="userData.age">
           </div>
         </div>
       </div>
@@ -23,18 +27,18 @@
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
           <label for="message">Message</label>
           <br>
-          <!-- Interpolation between <textarea>{{ test }}</textarea> doesn't work!-->
-          <textarea id="message" rows="5" class="form-control"></textarea>
+          <textarea id="message" rows="5" class="form-control" v-model="message"></textarea>
         </div>
       </div>
       <div class="row">
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
           <div class="form-group">
+            <!-- Check box  -->
             <label for="sendmail">
-              <input type="checkbox" id="sendmail" value="SendMail"> Send Mail
+              <input type="checkbox" id="sendmail" value="SendMail" v-model="sendMail"> Send Mail
             </label>
             <label for="sendInfomail">
-              <input type="checkbox" id="sendInfomail" value="SendInfoMail"> Send Infomail
+              <input type="checkbox" id="sendInfomail" value="SendInfoMail" v-model="sendMail"> Send Infomail
             </label>
           </div>
         </div>
@@ -42,18 +46,18 @@
       <div class="row">
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
           <label for="male">
-            <input type="radio" id="male" value="Male"> Male
+            <input type="radio" id="male" value="Male" v-model="sex"> Male
           </label>
           <label for="female">
-            <input type="radio" id="female" value="Female"> Female
+            <input type="radio" id="female" value="Female" v-model="sex"> Female
           </label>
         </div>
       </div>
       <div class="row">
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 from-group">
           <label for="priority">Priority</label>
-          <select id="priority" class="form-control">
-            <option></option>
+          <select id="priority" class="form-control" v-model="selectedPriority">
+            <option v-for="priority in priorities" >{{priority}}</option>
           </select>
         </div>
       </div>
@@ -69,21 +73,22 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <div class="panel panel-default">
           <div class="panel-heading">
-            <h4>Your Data</h4>
+            <h4>Your Information</h4>
           </div>
           <div class="panel-body">
-            <p>Mail:</p>
-            <p>Password:</p>
-            <p>Age:</p>
-            <p>Message:</p>
+            <p>Mail: {{userData.email}}</p>
+            <p>Password: {{userData.email}}</p>
+            <p>Age:{{userData.age}}</p>
+            <p style="white-space: pre">Message:{{message}}</p>
             <p>
               <strong>Send Mail?</strong>
             </p>
             <ul>
-              <li></li>
+                 
+              <li v-for="item in sendMail" >{{item}}</li>
             </ul>
-            <p>Gender:</p>
-            <p>Priority:</p>
+            <p>Gender: {{sex}}</p>
+            <p>Priority:{{selectedPriority}}</p>
             <p>Switched:</p>
           </div>
         </div>
@@ -94,7 +99,23 @@
 
 <script>
 export default {
-  name: "myForm"
+  name: "myForm",
+  
+  data() {
+    return {
+      userData: {
+        email: "",
+        password: "",
+        age: ""
+      },
+      message: "",
+      
+      sendMail:[],
+      sex:'Male',
+      selectedPriority:'High',
+      priorities:['Low','Normal','Medium','High']
+    };
+  }
 };
 </script>
 
