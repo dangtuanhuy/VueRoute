c81757825a0cc894061a5d0fb83d91332ad4d701
Huy Dang Tuan <huy.dang@orientsoftware.com>
2019-04-25 15:48:51 +0700

    Form

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-import BootstrapVue from 'bootstrap-vue'
-import VueRouter from 'vue-router';
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+// import BootstrapVue from 'bootstrap-vue'
+// import VueRouter from 'vue-router';
 Vue.config.productionTip = false
 
-Vue.ues(BootstrapVue)
-Vue.ues(VueRouter)
+// Vue.ues(BootstrapVue)
+// Vue.ues(VueRouter)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
