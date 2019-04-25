0db95473dc6a828de78335cef228f74b9be6f8de
Huy Dang Tuan <huy.dang@orientsoftware.com>
2019-04-25 14:43:04 +0700

    Save

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import BootstrapVue from 'bootstrap-vue'
+import VueRouter from 'vue-router';
 Vue.config.productionTip = false
 
+Vue.ues(BootstrapVue)
+Vue.ues(VueRouter)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
