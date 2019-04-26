1a80a9c8712f7ad7ed11d0d8ad692c431821b744
Huy Dang Tuan <huy.dang@orientsoftware.com>
2019-04-26 13:25:13 +0700

    datatable

--- src/components/datatableBootstraps.vue
+++ src/components/datatableBootstraps.vue
@@ -1,43 +1,63 @@
 <template>
   <div>
     <b-container class="bv-example-row">
-         <b-row>
-      <b-col md="6" class="my-1">
-        <b-form-group label-cols-sm="3" label="Filter" class="mb-0">
-          <b-input-group>
-            <b-form-input v-model="filter" placeholder="Type to Search"></b-form-input>
-            <b-input-group-append>
-              <b-button :disabled="!filter" @click="filter = ''">Clear</b-button>
-            </b-input-group-append>
-          </b-input-group>
-        </b-form-group>
-      </b-col>
-         </b-row>
+      <p class="mt-3">Current Page: {{ currentPage }}</p>
+      <b-row>
+        <b-col md="6" class="my-1">
+          <b-form-group label-cols-sm="3" label="Search" class="mb-0">
+            <b-input-group>
+              <b-form-input v-model="filter" placeholder="Type to Search"></b-form-input>
+              <b-input-group-append>
+                <b-button :disabled="!filter" @click="filter = ''">Clear</b-button>
+              </b-input-group-append>
+            </b-input-group>
+          </b-form-group>
+        </b-col>
+        <b-col md="6" class="my-1">
+          <b-form-group label-cols-sm="3" label="Filter" class="mb-0">
+            <b-dropdown id="dropdown-1" :text="selected" class="mb-0">
+              <b-dropdown-item v-for="item in options" @click="setPerPage(item)">{{item}}</b-dropdown-item>
+            </b-dropdown>
+          </b-form-group>
+        </b-col>
+      </b-row>
       <div class="overflow-auto">
-          
-        <p class="mt-3">Current Page: {{ currentPage }}</p>
-        <b-nav-form>
-          <b-form-input size="sm" class="mr-sm-2" placeholder="Search" v-model="filter"></b-form-input>
-          <b-button size="sm" class="my-2 my-sm-0" type="text">Search</b-button>
-        </b-nav-form>
-        <b-table id="my-table" :items="dataTable" :per-page="perPage" :current-page="currentPage" small>
-        </b-table>
+        <b-table bordered="1"
+          id="my-table"
+          :items="dataTable"
+          :per-page="perPage"
+          :current-page="currentPage"
+          small
+        ></b-table>
         <div>
           <b-container class="bv-example-row">
             <b-row>
               <b-col>
                 <b-pagination
+                  align="center"
                   v-model="currentPage"
                   :total-rows="rows"
                   :per-page="perPage"
+                  first-text="First"
+                  prev-text="Prev"
+                  next-text="Next"
+                  last-text="Last"
                   aria-controls="my-table"
-                ></b-pagination>
-              </b-col>
-              <b-col></b-col>
-              <b-col>
-                <b-dropdown id="dropdown-1" :text="selected" class="m-md-2">
-                  <b-dropdown-item v-for="item in options" @click="setPerPage(item)">{{item}}</b-dropdown-item>
-                </b-dropdown>
+                >
+                  <span class="text-info" slot="first-text">First</span>
+                  <span class="text-info" slot="prev-text">Prev</span>
+                  <span class="text-info" slot="next-text">Next</span>
+                  <span class="text-info" slot="last-text">Last</span>
+                  <div slot="ellipsis-text">
+                    <b-spinner small type="grow"></b-spinner>
+                    <b-spinner small type="grow"></b-spinner>
+                    <b-spinner small type="grow"></b-spinner>
+                  </div>
+                  <span slot="page" slot-scope="{ page, active }">
+                    <b v-if="active">{{ page }}</b>
+                    <i v-else>{{ page }}</i>
+                  </span>
+                </b-pagination>
               </b-col>
             </b-row>
           </b-container>
@@ -53,6 +73,7 @@ export default {
   data() {
     return {
       perPage: 5,
+
       currentPage: 1,
       items: [],
       options: [5, 10, 15, 20, 50],
